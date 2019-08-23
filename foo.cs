f5fcaa03364a7a7bba96a8930cb5b21e31dd7ce6
vsanchezm <vsanchezm@codicesoftware.com>
2019-08-23 11:25:49 +0000

    Other change in dst

--- foo.cs
+++ foo.cs
@@ -101,7 +101,7 @@ namespace GitMaster.LoginWindow
 
         Panel CreateContentErrorPanel(string message)
         {
-            StackPanel result = new StackPanel(); //other change method dst
+            StackPanel result = new StackPanel(); //other change method dst 2
 
             TextBlock titleTextBlock = WebControlBuilder.CreateTitle(
                 GitMasterLocalization.GetString(
